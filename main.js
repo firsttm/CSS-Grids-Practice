fd10d8e5cb1c1f5fdcff8089c1e3880b78d32bab
Ishan <seriouslyflabbergasted@gmail.com>
2017-08-14 14:28:29 +0530

    slideshow + stuff

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 console.log("started");
 console.log("second");
+
+
+// UNCOMMENT TILL  *  TO ENABLE SLIDESHOW 
+
+// $(document).ready(function(){
+    
+//     $('.jumbo > div:gt(0)').hide();
+    
+//     setInterval(function() {
+//       $('.jumbo > div:first')
+//         .fadeOut(1000)
+//         .next()
+//         .fadeIn(500)
+//         .end()
+//         .appendTo('.jumbo');
+//     }, 6000);
+// });
+
+//   * HERE
