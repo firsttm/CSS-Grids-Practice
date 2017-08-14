eff2a9ee393602379206e1298aa2633b1d5ff721
Ishan <seriouslyflabbergasted@gmail.com>
2017-08-14 19:33:23 +0530

    started slideshow

--- main.js
+++ main.js
@@ -4,18 +4,18 @@ console.log("second");
 
 // UNCOMMENT TILL  *  TO ENABLE SLIDESHOW 
 
-// $(document).ready(function(){
+$(document).ready(function(){
     
-//     $('.jumbo > div:gt(0)').hide();
+    $('.jumbo > div:gt(0)').hide();
     
-//     setInterval(function() {
-//       $('.jumbo > div:first')
-//         .fadeOut(1000)
-//         .next()
-//         .fadeIn(500)
-//         .end()
-//         .appendTo('.jumbo');
-//     }, 6000);
-// });
+    setInterval(function() {
+      $('.jumbo > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(500)
+        .end()
+        .appendTo('.jumbo');
+    }, 6000);
+});
 
 //   * HERE
